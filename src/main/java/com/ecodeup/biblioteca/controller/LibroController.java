0effeca755ced6a00f46fda64b76321f7a3cfb18
Ricardo Galarza <ricardogalarza1723@gmail.com>
2024-02-07 11:11:51 -0300

    Cambiando libroController

--- src/main/java/com/ecodeup/biblioteca/controller/LibroController.java
+++ src/main/java/com/ecodeup/biblioteca/controller/LibroController.java
@@ -1,8 +1,11 @@
 package com.ecodeup.biblioteca.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,4 +30,10 @@ public class LibroController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping
+    public ResponseEntity<List<LibroDTO>> findAll(){
+        return new ResponseEntity<>(libroService.findAll(),HttpStatus.OK);
+    }
+
 }
